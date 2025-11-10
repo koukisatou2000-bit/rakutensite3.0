a3fb9d474411dd76df611602bf43045d6a7e1d7e
koukisatou2000-bit <koukisatou2000-bit@users.noreply.github.com>
2025-11-11 06:59:13 +0900

    Use eventlet instead of gevent

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,22 +1,9 @@
 import os
 
-# ワーカータイムアウトを大幅に延長
 timeout = 300
-
-# geventワーカーを使用
-worker_class = 'gevent'
-
-# ワーカー数
+worker_class = 'eventlet'
 workers = 1
-
-# バインド
 bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
-
-# ログレベル
 loglevel = 'info'
-
-# アクセスログ
 accesslog = '-'
-
-# エラーログ
 errorlog = '-'
